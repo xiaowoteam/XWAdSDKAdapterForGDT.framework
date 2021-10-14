a02415157f71e54d7d2c7e69842d97ec51f94645
laole918 <laole918@qq.com>
2021-10-14 18:29:36 +0800

    first commit

---
+++ XWAdSDKAdapterForGDT.framework/Headers/XWAdSDKAdapterForGDT.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForGDT.h
+//  XWAdSDKAdapterForGDT
+//
+//  Created by laole918 on 2021/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForGDT.
+FOUNDATION_EXPORT double XWAdSDKAdapterForGDTVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForGDT.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForGDTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForGDT/PublicHeader.h>
+
+
